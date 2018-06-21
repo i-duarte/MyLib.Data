0391b3a50af3b61f7cd5093d41b5059dd4b7144b
IVAN DUARTE BRAVO <i_duarte@hotmail.com>
2018-06-20 21:28:37 -0700

    Daily

--- src/Demo.Data/Tables/Rows/Product.cs
+++ src/Demo.Data/Tables/Rows/Product.cs
@@ -1,9 +1,24 @@
 ﻿using MyLib.Data.Common;
+using MyLib.Data.EntityFramework;
+using MyLib.Data.EntityFramework.Attributes;
 
 namespace Demo.Data.Tables.Rows
 {
-	public partial class Product
+	public class Product : Entity
 	{
+
+		[Field(IsPrimaryKey = true)]
+		public int IdProduct { get; set; }
+
+		[Field]
+		public int IdBrand { get; set; }
+
+		[Field(Size = 50)]
+		public string Name { get; set; }
+
+		[Field(Size = 400)]
+		public string Description { get; set; }
+
 		public Product()
 		{
 		}
