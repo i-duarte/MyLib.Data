463509d1af9099cd03c731dda78e053d0fa784cd
i-duarte <32073676+i-duarte@users.noreply.github.com>
2024-05-08 14:13:23 -0700

    SE AGREGO FILTRO A LA GRAFICA DE PERFIL

--- src/MyLib.Web.Common/DataItem.cs
+++ src/MyLib.Web.Common/DataItem.cs
@@ -4,6 +4,16 @@
     {
         public object Id { get; set; }
         public string Description { get; set; }
+
+        public DataItem()
+        {
+        }
+
+        public DataItem(object id, string descripcion)
+        {
+            Id = id;
+            Description = descripcion;
+        }
     }
 
 
